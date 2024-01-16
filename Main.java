8c908d140c0d1387d6c7dd609d1a4ef90aec4de4
Liver S. <szabooli24@7hills.org>
2024-01-16 12:49:25 -0500

    array list of student objects

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Seminar[][] schedule = new Seminar[5][5];
+        ArrayList<Student> students = new ArrayList<Student>();
     }
 }
