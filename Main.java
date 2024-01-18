58e408901fb6a5c4eef93b58f427a529912b3697
liver2 <ilovemath896@gmail.com>
2024-01-18 12:19:17 -0500

    split data files, fileio

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
         Seminar[][] schedule = new Seminar[5][5];
+        ArrayList<String> dataStrings = new ArrayList<String>();
         ArrayList<Student> students = new ArrayList<Student>();
+
+        try {
+            File data = new File("actualSrSeminarData.csv");
+            Scanner scan = new Scanner(data);
+            scan.nextLine();
+            while(scan.hasNextLine()) {
+                dataStrings.add(scan.nextLine());
+            }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+        
+        System.out.println(dataStrings);
     }
 }
