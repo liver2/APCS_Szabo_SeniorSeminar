2227736eb2c2d8ce43d6ac371ce2efb2ea9ef71e
liver2 <124078178+liver2@users.noreply.github.com>
2024-01-24 22:22:05 -0500

    duration

--- Main.java
+++ Main.java
@@ -2,12 +2,15 @@ public class Main {
     public static void main(String[] args) {
         ScheduleOp scheduleOp = new ScheduleOp();
         int i = 1000; 
+        long start = System.nanoTime();
         while (true) {
             int compare = scheduleOp.randomize();
             if (compare < i)
             {
                 i = compare;
-                System.out.println("New best optimization score " + i + "found");
+                long point = System.nanoTime();
+                long duration = (point - start)/1000000000;
+                System.out.println("New best optimization score " + i + " found at " + duration + "s");
             }
         }
     }
