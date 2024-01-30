11c523f267af5a1441f7f194a6621d9c5129d08d
Oliver Szabo <szabooli24@7hills.org>
2024-01-30 13:15:27 -0500

    add seminar id -1 disclaimer

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("For students who did not fill out the form, a Seminar ID of \"-1\" indicates we were unable to place the student in a Seminar.");
         ScheduleOp scheduleOp = new ScheduleOp();
         Scanner scan = new Scanner(System.in);
         scheduleOp.randomize();
